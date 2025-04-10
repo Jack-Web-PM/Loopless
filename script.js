501e95f96f32e7c8f29794595362b03b6b44de5c
Jack-Web-PM <jack.chou24@student.cart.org>
2025-04-10 13:00:34 -0700

    Loopless

---
+++ script.js
@@ -0,0 +1,10 @@
+function loopless(count) {
+    const numbers = [];
+    for (let i = 1; i <= count; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+// const loopLess = loopless(1000);
+// console.log(loopLess);
